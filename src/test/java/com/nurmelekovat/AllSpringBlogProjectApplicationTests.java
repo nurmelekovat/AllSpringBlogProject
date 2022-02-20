e0cbc976e000d2a26f87b19e5755e24466c02575
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-20 16:52:02 +0300

    Initial commit

---
+++ src/test/java/com/nurmelekovat/AllSpringBlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurmelekovat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSpringBlogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
